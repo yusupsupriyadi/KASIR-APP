5a164ddda85079467d466e0a8f105215abb07822
yusupsupriyadi <yusupsupriyadi242@gmail.com>
2021-01-20 23:34:49 +0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Hasil from './Hasil'
+import ListCategories from './ListCategories'
+import NavbarComponent from './NavbarComponent'
+import TotalBayar from './TotalBayar'
+import ModalKeranjang from './ModalKeranjang'
+import Menus from './Menus'
+
+export { Hasil, ListCategories, NavbarComponent, Menus, TotalBayar, ModalKeranjang}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+// import Crud from './crud';
+// import Lifecycle from './react-dasar/Lifecycle';
+// import Map from './react-dasar/Map'
+// import StateProps from './react-dasar/StateProps';
+// import Variabel from './react-dasar/Variabel';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +18,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
